9f3a2314965e1c70c9a0e8d184e38537cc651c5e
Ioannis Petropoulos <32548818+ionpetro@users.noreply.github.com>
2017-11-05 21:07:23 +0200

    Update SCANNERSPLITTEST.java
    What I did here, is that I added the method "replaceAll" that removes the punctuation from the string.

--- SCANNERSPLITTEST.java
+++ SCANNERSPLITTEST.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class SCANNERSPLITTEST {
 
 	public static void main ( String[] args) {
-		Scanner input = new Scanner(System.in);
-		String str = input.nextLine();
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		String strippedInput = input.replaceAll("\\W ", " ");
 		String array[] = new String[1000];
 		int i = 0;
-		Scanner fromStr = new Scanner(str);
+		Scanner fromStr = new Scanner(strippedInput);
 		while (fromStr.hasNext()) {
 			array[i]=fromStr.next();
 			i++;
