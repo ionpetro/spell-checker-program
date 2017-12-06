af6ceb11ffc8bf8e7b58f56c2511c77e7549a1f0
PalivakouOlga <32675900+PalivakouOlga@users.noreply.github.com>
2017-12-06 18:27:55 +0200

    Delete GreekChars.java

--- GreekChars.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class GreekChars {
-	public static void ourText(){
-        Scanner keyboard = new Scanner(System.in, "utf-8");
-        System.out.println("Enter your text");
-        String myText = keyboard.nextLine();
-        System.out.println(myText);
-	}
-}
