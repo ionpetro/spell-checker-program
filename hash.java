a6a84acaa04466385b7dfc77ad0451725e4f2076
athbol <32675046+athbol@users.noreply.github.com>
2017-12-07 04:23:11 +0200

    Update hash.java

--- hash.java
+++ hash.java
@@ -4,11 +4,19 @@ import java.io.*;
 
 public class hash {
 
-	public static HashSet<String> dictionary = new HashSet<String>();  // the dictionary used
+
+	//list of suggested words//
+	public static List<String> suggested = new ArrayList<String>();
+
+	// the dictionary used//
+	public static HashSet<String> dictionary = new HashSet<String>();
+
 
 	public static void main (String[]args){
 		int tableSIZE=10000000;
   		dictionary = new HashSet<String>(tableSIZE);
+
+		//read dictionary
 		try {
 			BufferedReader bufferedReader = new BufferedReader(new FileReader("./dictionary.txt"));
 	        String line = null; // notes one line at a time
@@ -21,23 +29,36 @@ public class hash {
 		}
 
 		/*print whole dictionary//
-				for (String temp : dictionary) {
-						        System.out.println(temp);
+		for (String temp : dictionary) {
+			System.out.println(temp);
     	}
     	*/
 
-
-			ArrayList<String> list = new ArrayList<String>();
-			list.add("harvest");
-			list.add("chair");
-			list.add("locally");
+		//a list that is filled with some text to spell check
+		ArrayList<String> list = new ArrayList<String>();
+			list.add("computer");
 
 
-			for(int i=0;i<list.size();i++) {
-				if(dictionary.contains(new String(list.get(i)))) {
-			System.out.println("true");
-			}}
 
+
+
+		int i=0;
+
+		//checking words that are similar to each element of the list and putting them into a suggested list to be displayed
+		for(int j=0;j<list.size();j++) {
+			for(String s : dictionary){
+		     	i = Levenshtein.distance(s, list.get(j));
+		     	if(i == 0) {
+					System.out.println("as");
+		     	}else if(i<2){
+		          suggested.add(s);
+		     	}
+        	}
+		}
+
+		for(String a : suggested) {
+		            System.out.println(a);
+		}
 
 	}
 }
