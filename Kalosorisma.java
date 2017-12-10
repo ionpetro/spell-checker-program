57e38168b142fe0207bce4117aa433cf045b80fa
lchristoforatos <32678592+lchristoforatos@users.noreply.github.com>
2017-12-10 23:09:20 +0200

    Add files via upload

---
+++ Kalosorisma.java
@@ -0,0 +1,10 @@
+public class Kalosorisma {
+
+	public static void main(String[] args) {
+
+		System.out.println("Agaphte admin kalwsorises sto programma orthografikou elegxou ths omadas mas pou onomazetai javaholics.");
+		System.out.println("To programma mas ousiastika einai enas elegxos pou dexetai ena keimeno , tonizei tis orthografikes tou adynamies kai proteinei enallaktikes stis lathos lexeis");
+		System.out.println("Yposthrizontai h ellhnikh kai h agglikh glwssa");
+		System.out.println("Elpizoume na mporesoume na se voithisoume");
+	}
+}
